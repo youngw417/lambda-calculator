2262c0e360e2771122431ae68bb076fb69f6c5d6
Young Won <youngw417@gmail.com>
2019-11-05 22:30:56 -0800

    component-calculator

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+
+      <button className = 'button' onClick = {() => props.setNum(props.num + props.button)}>
+        {props.button}
+      </button>
+      }
     </>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props */
+      <button onClick = {() => {if (props.ops === 'C') {props.setNum('')} else
+      if (props.ops === '%') {props.setNum(props.num / 100)}
+      else props.setNum(props.num * -1) }}>{props.ops}</button>
+      }
     </>
   );
 };
+
+export default SpecialButton;
